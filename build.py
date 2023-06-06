8fcd509265431e87a490be790e8c81045b0cc8e0
Masahiro Masuda <masahi129@gmail.com>
2023-06-06 18:19:24 +0900

    skip tir dispatch

--- build.py
+++ build.py
@@ -356,18 +356,19 @@ def build(mod_deploy: tvm.IRModule, args: argparse.Namespace) -> None:
 
     debug_dump_script(mod_deploy, "mod_before_build.py", args)
     if target_kind != "cpu":
-        db = utils.get_database(args.db_path)  # pylint: disable=invalid-name
-        with db, tvm.target.Target("apple/m1-gpu-restricted"):
-            if args.target_kind == "android":
-                mod_deploy = mlc_llm.dispatch.DispatchTIROperatorAdreno()(  # pylint: disable=not-callable
-                    mod_deploy
-                )
-            mod_deploy = relax.transform.MetaScheduleApplyDatabase()(mod_deploy)
-            mod_deploy = (
-                mlc_llm.dispatch.DispatchTIROperator(  # pylint: disable=not-callable
-                    args.model_category
-                )(mod_deploy)
-            )
+        # db = utils.get_database(args.db_path)  # pylint: disable=invalid-name
+        # with db, tvm.target.Target("apple/m1-gpu-restricted"):
+        #     if args.target_kind == "android":
+        #         mod_deploy = mlc_llm.dispatch.DispatchTIROperatorAdreno()(  # pylint: disable=not-callable
+        #             mod_deploy
+        #         )
+        #     mod_deploy = relax.transform.MetaScheduleApplyDatabase()(mod_deploy)
+        #     mod_deploy = (
+        #         mlc_llm.dispatch.DispatchTIROperator(  # pylint: disable=not-callable
+        #             args.model_category
+        #         )(mod_deploy)
+        #     )
+        with tvm.target.Target(target_kind):
             mod_deploy = tvm.tir.transform.DefaultGPUSchedule()(mod_deploy)
             mod_deploy = mlc_llm.transform.LiftTIRGlobalBufferAlloc()(mod_deploy)
             mod_deploy = tvm.tir.transform.ForceNarrowIndexToInt32()(mod_deploy)
